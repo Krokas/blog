eb15ea12bc232aa1538fb1453ae905d369362bb9
krokas <rokas.kondrackis@gmail.com>
2022-01-01 09:46:42 +0200

    auth guard for admin space and login controller

--- routes/admin.php
+++ routes/admin.php
@@ -1,8 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\UserController;
 
 
-Route::get('/', function() {
-    return view('welcome');
+
+Route::get('login', [UserController::class, 'login'])->name('admin.login');
+Route::middleware('auth')->group(function() {
+    Route::get('/', function() {
+        return view('welcome');
+    });
 });
