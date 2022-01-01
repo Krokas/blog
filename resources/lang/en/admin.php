2c91986d823e0810bdb4ae7fe317a84903f16770
krokas <rokas.kondrackis@gmail.com>
2022-01-01 10:48:40 +0200

    login form

---
+++ resources/lang/en/admin.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'login' => [
+        'h1' => 'Login',
+        'email' => 'Email',
+        'password' => 'Password',
+        'submit' => 'Log in'
+    ]
+];
--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,4 +1,22 @@
 @extends('shared.templates.admin')
 @section('content')
-    <h1>Please login</h1>
+    <div class="login__page">
+        <div class="login__container">
+            <h1 class="mb-3">@lang('admin.login.h1')</h1>
+            <form method="POST">
+                <div class="mb-3">
+                    <label for="email" class="label">@lang('admin.login.email')</label>
+                    <input type="email" name="email" class="form-control">
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="label">@lang('admin.login.password')</label>
+                    <input type="password" name="password" class="form-control">
+                </div>
+                <div class="mb-3">
+                    @csrf
+                    <input type="submit" value="{{ __('admin.login.submit') }}" class="btn btn-primary">
+                </div>
+            </form>
+        </div>
+    </div>
 @endsection
