c9089d61aacac1bdc024aa1eac985ae5250f4ead
krokas <rokas.kondrackis@gmail.com>
2022-03-15 22:23:59 +0200

    Fix tinymce default style_formats

--- resources/js/editor.js
+++ resources/js/editor.js
@@ -6,12 +6,57 @@ import "tinymce/skins/ui/oxide/content.min.css";
 import "tinymce/skins/content/default/content.css";
 import "tinymce/plugins/autoresize/plugin";
 
+const styleFormats = [
+    {
+        title: "Headings",
+        items: [
+            { title: "Heading 1", format: "h1" },
+            { title: "Heading 2", format: "h2" },
+            { title: "Heading 3", format: "h3" },
+            { title: "Heading 4", format: "h4" },
+            { title: "Heading 5", format: "h5" },
+            { title: "Heading 6", format: "h6" },
+        ],
+    },
+    {
+        title: "Inline",
+        items: [
+            { title: "Bold", format: "bold" },
+            { title: "Italic", format: "italic" },
+            { title: "Underline", format: "underline" },
+            { title: "Strikethrough", format: "strikethrough" },
+            { title: "Superscript", format: "superscript" },
+            { title: "Subscript", format: "subscript" },
+            { title: "Code", format: "code" },
+        ],
+    },
+    {
+        title: "Blocks",
+        items: [
+            { title: "Paragraph", format: "p" },
+            { title: "Blockquote", format: "blockquote" },
+            { title: "Div", format: "div" },
+            { title: "Pre", format: "pre" },
+        ],
+    },
+    {
+        title: "Align",
+        items: [
+            { title: "Left", format: "alignleft" },
+            { title: "Center", format: "aligncenter" },
+            { title: "Right", format: "alignright" },
+            { title: "Justify", format: "alignjustify" },
+        ],
+    },
+];
+
 tinymce.init({
     selector: '[name="body"]',
     theme: "silver",
     menubar: false,
     plugins: "autoresize",
     min_height: 700,
+    style_formats: styleFormats,
     setup: (editor) => {
         editor.on("keyup", () => {
             clearTimeout(typingTimer);
